61206f3d8ffbea8f0d69fe946a01a3ef5fd3d124
Volkan <volkanitmec0@gmx.de>
2025-09-16 19:53:43 +0200

    Added a function to load all files for other modules

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# For loading only one batch; Not needed anymore
 def load_dat_file(filepath):
     data = []
     with open(filepath, 'r') as file:
@@ -10,3 +11,14 @@ def load_dat_file(filepath):
             features["label"] = label
             data.append(features)
     return pd.DataFrame(data)
+
+# For loading all batches
+def load_all_batches(folderpath, batches=10):
+    dfs = []
+    for i in range(1, batches+1):
+        filepath = fr"{folderpath}\batch{i}.dat"
+        df_temp = load_dat_file(filepath)
+        df_temp["batch"] = f"batch{i}"
+        dfs.append(df_temp)
+    combined_df = pd.concat(dfs, ignore_index=True)
+    return combined_df
